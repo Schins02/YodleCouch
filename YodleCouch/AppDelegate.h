98a75f02da5bb74b7b4397c532f6b6cf50ee2b0f
John Schindler <john.schindler@utexas.edu>
2015-07-22 14:35:06 -0500

    adding auto-gen proj files

---
+++ YodleCouch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YodleCouch
+//
+//  Created by John Schindler on 7/22/15.
+//  Copyright (c) 2015 Yodle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YodleCouch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YodleCouch
+//
+//  Created by John Schindler on 7/22/15.
+//  Copyright (c) 2015 Yodle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
